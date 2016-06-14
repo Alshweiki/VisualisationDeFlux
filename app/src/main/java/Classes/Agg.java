ce156c940f7cb5e7e80707b7d28bf5c524017194
Alshweiki <ali.shwaiky@gmail.com>
2016-06-14 17:01:52 +0200

    Classe Agg

--- app/src/main/java/Classes/Agg.java
+++ app/src/main/java/Classes/Agg.java
@@ -11,5 +11,31 @@ public class Agg implements Serializable {
     private Date timestamp;
     private int antenneId;
     private float value;
-    
+
+    public float getValue() {
+        return value;
+    }
+
+    public void setValue(float value) {
+        this.value = value;
+    }
+
+    public int getAntenneId() {
+        return antenneId;
+    }
+
+    public void setAntenneId(int antenneId) {
+        this.antenneId = antenneId;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+
+
 }
