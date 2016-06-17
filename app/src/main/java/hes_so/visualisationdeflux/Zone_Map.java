40c99b5693e8289392e37fc468849ccd0037a9c6
Alshweiki <ali.shwaiky@gmail.com>
2016-06-17 12:00:25 +0200

    add zone square over the map

--- app/src/main/java/hes_so/visualisationdeflux/Zone_Map.java
+++ app/src/main/java/hes_so/visualisationdeflux/Zone_Map.java
@@ -2,6 +2,7 @@ package hes_so.visualisationdeflux;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
@@ -10,12 +11,28 @@ import android.support.design.widget.FloatingActionButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.List;
+
+import Classes.Zone;
 
 
 // In this case, the fragment displays simple text based on the page
@@ -23,6 +40,8 @@ public class Zone_Map extends android.support.v4.app.Fragment implements OnMapRe
     public static final String ARG_PAGE = "ARG_PAGE";
     private int mPage;
     private GoogleMap googleMap;
+    RequestQueue queue;
+    GoogleMap map;
 
     public static Zone_Map newInstance(int page) {
         Bundle args = new Bundle();
@@ -43,7 +62,7 @@ public class Zone_Map extends android.support.v4.app.Fragment implements OnMapRe
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_zone__map, container, false);
-
+        queue = Volley.newRequestQueue(view.getContext());
 
         FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -58,8 +77,56 @@ public class Zone_Map extends android.support.v4.app.Fragment implements OnMapRe
 
         SupportMapFragment mapFragment = (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.zonemap);
         mapFragment.getMapAsync(this);
-        GoogleMap map = mapFragment.getMap();
+        map = mapFragment.getMap();
         map.getUiSettings().setRotateGesturesEnabled(false);
+
+
+        String url ="http://iuam.tk/api/zones";
+        final StringRequest zonepos = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Gson gson = new Gson();
+                        List<Zone> zones = gson.fromJson(response,new TypeToken<List<Zone>>(){}.getType());
+
+                        //  Toast.makeText(getContext(),
+                        //        zones.size(),
+                        //        Toast.LENGTH_LONG).show();
+
+                        for(int i = 0;i<zones.size();i++) {
+                            if(zones.get(i).getType().toString().equals("dynamic"))
+                            {PolygonOptions rectOptions = new PolygonOptions()
+                                    .add(new LatLng(zones.get(i).getLat1(),zones.get(i).getLng1()),
+                                            new LatLng(zones.get(i).getLat1(),zones.get(i).getLng2()),
+                                            new LatLng(zones.get(i).getLat2(),zones.get(i).getLng2()),
+                                            new LatLng(zones.get(i).getLat2(),zones.get(i).getLng1()))
+                                    .strokeColor(Color.BLUE)
+                                    .fillColor(0x7F0000FF);
+                            Polygon polygon = map.addPolygon(rectOptions);}
+                            else
+                            {
+                                PolygonOptions rectOptions = new PolygonOptions()
+                                        .add(new LatLng(zones.get(i).getLat1(),zones.get(i).getLng1()),
+                                                new LatLng(zones.get(i).getLat1(),zones.get(i).getLng2()),
+                                                new LatLng(zones.get(i).getLat2(),zones.get(i).getLng2()),
+                                                new LatLng(zones.get(i).getLat2(),zones.get(i).getLng1()))
+                                        .strokeColor(Color.GREEN)
+                                        .fillColor(0x7F00FF00);
+                                Polygon polygon = map.addPolygon(rectOptions);
+                            }
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getContext(),
+                        error.toString(),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        queue.add(zonepos);
         return view;
     }
 
@@ -69,7 +136,7 @@ public class Zone_Map extends android.support.v4.app.Fragment implements OnMapRe
        // LatLng sydney = new LatLng(-34, 151);
       //  map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
        // map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 10));
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(46.99225448705687, 6.9419696554541), 7.0f));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(46.99225448705687, 6.9419696554541), 8.0f));
     }
 }
 
